174214b99656c877a6321244a223473953d8608b
Leonardo Aguilera Castro <leoac92@gmail.com>
2023-10-10 20:08:54 -0300

    first commi

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/navbar/Header'
+
+function App() {
+  
+  return (
+    <>
+      <header>
+      <NavBar/>
+      </header>
+    </>
+  )
+}
+
+export default App
